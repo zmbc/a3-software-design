aaf168a0441e4ed75bc41a75207cda60c69e6767
CamD67 <cameron.doane@gmail.com>
2017-05-09 17:36:25 -0700

    fix issue with attr

--- circlePack.js
+++ circlePack.js
@@ -1,41 +1,39 @@
 function circlePack(){
-    // nest all data in an obj
-    var margin = {
-        top: 10,
-        right: 10,
-        bottom: 10,
-        left: 10
-    };
-    var width = 800;
-    var height = 800;
-    var _drawWidth = width - margin.left - margin.right;
-    var _drawHeight = height - margin.top - margin.bottom;
-    var circlePadding = 2;
-    var minColor = "#333";
-    var maxColor = "#F0F";
-    var leafColor = "#0FF";
-    var strokeWidth = 2;
-    var strokeColor = "#000";
+    var attrs = {
+        margin: {
+            top: 10,
+            right: 10,
+            bottom: 10,
+            left: 10
+        },
+        width: 800,
+        height: 800,
+        circlePadding: 2,
+        minColor: "#333",
+        maxColor: "#F0F",
+        leafColor: "#0FF",
+        strokeWidth: 2,
+        strokeColor: "#000"
+    }
+    var _drawWidth = attrs.width - attrs.margin.left - attrs.margin.right;
+    var _drawHeight = attrs.height - attrs.margin.top - attrs.margin.bottom;
 
     var _colorScale = d3.scaleLinear()
         .domain([-1, 10])
-        .range([minColor, maxColor])
+        .range([attrs.minColor, attrs.maxColor])
         .interpolate(d3.interpolateRgb);
 
     var _pack = d3.pack()
         .size([_drawWidth, _drawWidth])
-        .padding(circlePadding);
+        .padding(attrs.circlePadding);
 
     var vis = function(selection){
-        this.width = width;
         _reset();
         selection.each(function(data, i){
-            console.log("CALLED VIS")
-            console.log(width);
             var svg = selection
                 .append("svg")
-                .attr("width", width)
-                .attr("height", height);
+                .attr("width", attrs.width)
+                .attr("height", attrs.height);
             var g = svg.append("g")
                 .attr("width", _drawWidth)
                 .attr("height", _drawHeight);
@@ -49,16 +47,14 @@ function circlePack(){
             circle.enter()
                 .append("circle")
                 .attr("class", function(d){ return d.children ? "circle-pack-node" : "circle-pack-leaf"; })
-                .style("stroke", strokeWidth)
+                .style("stroke", attrs.strokeWidth)
                 .on("mouseover", function(d){
-                    console.log("mouseover");
-                    d3.select(this).style("stroke", strokeColor);
+                    d3.select(this).style("stroke", attrs.strokeColor);
                 })
                 .on("mouseout", function(d){
-                    console.log("mouseout");
                     d3.select(this).style("stroke", "");
                 })
-                .style("fill", function(d){ return d.children ? _colorScale(d.depth) : leafColor; })
+                .style("fill", function(d){ return d.children ? _colorScale(d.depth) : attrs.leafColor; })
                 .attr("r", function(d){ return d.r; })
                 .attr("cx", function(d){ return d.x; })
                 .attr("cy", function(d){ return d.y; })
@@ -71,7 +67,7 @@ function circlePack(){
             console.warn("No arguments provided to attr method");
             return this;
         }
-        if(attr == undefined || attr == null || attr.length == 0 || this.hasOwnProperty(attr)){
+        if(attr == undefined || attr == null || attr.length == 0 || !attrs.hasOwnProperty(attr)){
             console.error("Improper attr: \"" + attr +"\" provided to circlePack.attr - Please use a string representing the attribute you'd like to modify");
             return this;
         }
@@ -79,29 +75,23 @@ function circlePack(){
             console.warn("Please do not modify private attributes");
             return this;
         }
-        if(arguments.length < 2) { return this[attr]; }
-        this[attr] = val;
-        console.log("CALLED ATTR")
-        console.log(this);
-        console.log(this[attr]);
-        console.log(width);
+        if(arguments.length < 2) { return attrs[attr]; }
+        attrs[attr] = val;
         return this;
     }
 
     // Reset any calculated variables, such as functions or values
     function _reset() {
-        console.log("called reset");
-        console.log(width);
-        _drawWidth = width - margin.left - margin.right;
-        _drawHeight = height - margin.top - margin.bottom;
+        _drawWidth = attrs.width - attrs.margin.left - attrs.margin.right;
+        _drawHeight = attrs.height - attrs.margin.top - attrs.margin.bottom;
         _colorScale = d3.scaleLinear()
             .domain([-1, 10])
-            .range([minColor, maxColor])
+            .range([attrs.minColor, attrs.maxColor])
             .interpolate(d3.interpolateRgb);
 
         _pack = d3.pack()
             .size([_drawWidth, _drawWidth])
-            .padding(circlePadding);
+            .padding(attrs.circlePadding);
     }
 
     return vis;
