aa50496f435994b0a6cf75c6edc442d0c70b4809
CamD67 <cameron.doane@gmail.com>
2017-05-09 21:45:08 -0700

    add zooming

--- circlePack.js
+++ circlePack.js
@@ -40,16 +40,17 @@ function circlePack(){
                 .attr("height", attrs.height);
             var g = svg.append("g")
                 .attr("width", _drawWidth)
-                .attr("height", _drawHeight);
+                .attr("height", _drawHeight)
+                .attr("transform", "translate(" + (_drawWidth / 2) + "," + (_drawHeight / 2) + ")");
             var root = d3.hierarchy(data, function(d) { return d[childAcc]; })
                 .sum(function(d){return d[sizeAcc]; })
                 .sort(function(a,b){return b.value - a.value;})
             var focus = root;
+            var view;
             var allNodes = _pack(root).descendants();
-            console.log(allNodes);
 
             var circle = g.selectAll("circle").data(allNodes);
-            circle.enter()
+            circle = circle.enter()
                 .append("circle")
                 .attr("class", function(d){ return d.children ? "circle-pack-node" : "circle-pack-leaf"; })
                 .style("stroke-width", attrs.strokeWidth)
@@ -59,22 +60,68 @@ function circlePack(){
                 .on("mouseout", function(d){
                     d3.select(this).style("stroke", "");
                 })
+                .on("click", function(d) {
+                        if(focus != d) {
+                            if(!d.children) {
+                                zoom(d.parent);
+                            } else {
+                                zoom(d);
+                            }
+                            d3.event.stopPropagation();
+                        }
+                    })
                 .style("fill", function(d){ return d.children ? _colorScale(d.depth) : attrs.leafColor; })
-                .attr("cx", function(d){ return d.x; })
-                .attr("cy", function(d){ return d.y; })
+                //.attr("cx", function(d){ return d.x; })
+                //.attr("cy", function(d){ return d.y; })
                 .attr("r", 0)
                 .transition()
                 .duration(attrs.animationDuration)
                 .attr("r", function(d){ return d.r; })
                 ;
+
             var text = g.selectAll("text").data(allNodes);
             text.enter()
                 .append("text")
                 .attr("class", "circle-pack-text")
-                .style("fill-opacity", function(d) { return d.parent === root ? 1 : 0; })
+                .style("text-anchor", "middle")
+                .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
+                .style("text-shadow", "0 1px 0 #fff, 1px 0 0 #fff, -1px 0 0 #fff, 0 -1px 0 #fff")
+                .style("display", function(d) { return d.parent === root ? "inline" : "none"; })
                 .text(function(d) { return d.data[nameAcc]; })
-                .attr("x", function(d) { return d.x; })
-                .attr("y", function(d) { return d.y; })
+                //.attr("x", function(d) { return d.x; })
+                //.attr("y", function(d) { return d.y; })
+                ;
+
+            svg.on("click", function() { zoom(root); });
+            var everything = g.selectAll("circle, text");
+
+            zoomTo([root.x, root.y, root.r * 2 + attrs.margin.top]);
+
+            // Main zoom functionality modified from https://bl.ocks.org/mbostock/7607535
+            function zoom(d) {
+                var focus0 = focus;
+                focus = d;
+
+                var transition = d3.transition()
+                    .duration(d3.event.altKey ? attrs.animationDuration * 5 : attrs.animationDuration)
+                    .tween("zoom", function(d) {
+                        var i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2 + attrs.margin.top]);
+                        return function(t) { zoomTo(i(t)); };
+                    });
+
+                transition.selectAll("text")
+                .filter(function(d) { return d.parent === focus || this.style.display === "inline"; })
+                    .style("fill-opacity", function(d) { return d.parent === focus ? 1 : 0; })
+                    .on("start", function(d) { if (d.parent === focus) this.style.display = "inline"; })
+                    .on("end", function(d) { if (d.parent !== focus) this.style.display = "none"; });
+            }
+
+            function zoomTo(v) {
+                var k = _drawWidth / v[2];
+                view = v;
+                everything.attr("transform", function(d) { return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")"; });
+                g.selectAll("circle").attr("r", function(d) { return d.r * k; });
+            }
         });
     }
 
@@ -122,6 +169,7 @@ function circlePack(){
         attrs[attr] = val;
         return this;
     }
+
 
     // Reset any calculated variables, such as functions or values
     function _reset() {
